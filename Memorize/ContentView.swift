490805bb96ba267a0e03e8be4ce8c2aafdb079ff
x6ogdasha <6ogdashaa@gmail.com>
2024-01-23 22:53:55 +0300

    refacor: added base constant

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -22,17 +22,18 @@ struct ContentView: View {
 
 struct CardView: View{
     var isFaceUp: Bool = false
+    
     var body: some View{
         ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            
             if isFaceUp{
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 5)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 5)
                 Text("🐻")
                     .font(.largeTitle)
             } else{
-                RoundedRectangle(cornerRadius: 12)
+                base
             }
         }.frame(height: 100)
     }
