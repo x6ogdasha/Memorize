6637e4b0ae1c39d509f6319aa591d989330a2224
x6ogdasha <6ogdashaa@gmail.com>
2024-02-01 14:49:33 +0300

    refactor: HStack changed to LazyVGrid

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
     }
     
     var cards: some View{
-        HStack (){
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]){
             ForEach(0..<cardCount, id: \.self){ index in
                 CardView(content: emojis[index])
             }
