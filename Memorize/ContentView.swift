95411d912b93f0480b6a68678c96e8b168512195
x6ogdasha <6ogdashaa@gmail.com>
2024-02-01 14:52:11 +0300

    feat: added func for creating a button, optimized same button vars

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -40,19 +40,22 @@ struct ContentView: View {
             cardAdder
         }.font(.title)
     }
-    var cardRemover: some View{
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View{
         Button(action:{
-            if (cardCount > 1) { cardCount -= 1 }
+            cardCount += offset
         }, label:{
-            Image(systemName: "rectangle.stack.badge.minus.fill")
+            Image(systemName: symbol)
         })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
     }
+    
+    var cardRemover: some View{
+       cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
     var cardAdder: some View{
-        Button(action:{
-            if (cardCount < emojis.count) { cardCount += 1 }
-        }, label:{
-            Image(systemName: "rectangle.stack.badge.plus.fill")
-        })
+        cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus.fill")
     }
 }
 
