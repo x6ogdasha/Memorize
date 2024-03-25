a4856b47f4eddf6074f4d836cd68c05a0ce70b7e
Bogdan Kitaev <6ogdashaa@gmail.com>
2024-03-25 10:18:04 +0300

    feat: added Pie shape for card

---
+++ Memorize/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Memorize
+//
+//  Created by Богдан Китаев on 24.02.2024.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
