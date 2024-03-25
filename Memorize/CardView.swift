e0b08f72c190be4871539ad3830ad15f70d3ece5
Bogdan Kitaev <6ogdashaa@gmail.com>
2024-03-25 10:19:09 +0300

    feat: added a Card View

--- Memorize/CardView.swift
+++ Memorize/CardView.swift
@@ -8,11 +8,68 @@
 import SwiftUI
 
 struct CardView: View {
+    typealias Card = MemoryGame<String>.Card
+    let card: Card
+    
+    init(_ card: MemoryGame<String>.Card) {
+        self.card = card
+    }
+    
+    private struct Constants{
+        static let cornerRadius: CGFloat = 12
+        static let lineWidth: CGFloat = 2
+        static let inset: CGFloat = 5
+        struct FontSize {
+            static let largest: CGFloat = 200
+            static let smallest: CGFloat = 10
+            static let scaleFactor = smallest / largest
+        }
+        struct Pie {
+            static let opacity: CGFloat = 0.5
+            static let inset: CGFloat = 5
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: Constants.cornerRadius)
+            
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: Constants.lineWidth)
+                Pie(endAngle: .degrees(240))
+                    .opacity(Constants.Pie.opacity)
+                    .overlay(
+                        Text(card.content)
+                            .font(.system(size: Constants.FontSize.largest))
+                            .minimumScaleFactor(Constants.FontSize.scaleFactor )
+                            .multilineTextAlignment(.center)
+                            .aspectRatio(1, contentMode: .fit)
+                            .padding(Constants.Pie.inset)
+                    )
+                    .padding(Constants.inset)
+            }
+            .opacity(card.isFaceUp ? 1 : 0)
+            base.fill()
+            .opacity(card.isFaceUp ? 0 : 1)
+        }
+        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
     }
 }
 
+
+
 #Preview {
-    CardView()
+    VStack{
+        HStack{
+            CardView(MemoryGame<String>.Card(isFaceUp: true, content: "x", id: "test"))
+            CardView(MemoryGame<String>.Card(content: "x", id: "test"))
+        }
+        HStack{
+            CardView(MemoryGame<String>.Card(isFaceUp: true, isMatched: true, content: "🥸", id: "test"))
+            CardView(MemoryGame<String>.Card(isMatched: true, content: "x",  id: "test"))
+        }
+    }
+    .padding()
+    .foregroundColor(.gray)
 }
