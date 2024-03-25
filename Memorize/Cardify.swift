1d8ffe90ee7d6736fbf7bbfcc58cadcb1433caff
Bogdan Kitaev <6ogdashaa@gmail.com>
2024-03-25 10:32:14 +0300

    feat: added Cardify (card modifier)

---
+++ Memorize/Cardify.swift
@@ -0,0 +1,35 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by Богдан Китаев on 25.03.2024.
+//
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    
+    let isFaceUp: Bool
+    
+    func body(content: Content) -> some View {
+       
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: Constants.cornerRadius)
+            
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: Constants.lineWidth)
+                content
+            }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill()
+            .opacity(isFaceUp ? 0 : 1)
+        }
+    }
+    
+    private struct Constants{
+        
+        static let cornerRadius: CGFloat = 12
+        static let lineWidth: CGFloat = 2
+    }
+}
